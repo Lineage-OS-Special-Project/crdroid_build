e7d88e5b12bc2c7d6bd2434f0ec514cfe73204ec
yehonatan2020 <yehonatan@yandex.ru>
2024-07-08 10:19:26 +0100

    product: Disable traced persistence by default
    This change sets the default value of persist.traced.enable to 0,
    disabling the system tracing daemon (traced) from persisting across
    reboots.
    
    While the tracing infrastructure is valuable for debugging, it is not
    needed for the majority of users and can be safely disabled to
    conserve system resources. Developers and users who require system
    tracing can still enable it manually as needed.
    
    Signed-off-by: Yehonatan5785 <yehonatan@mail.ru>

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -516,7 +516,7 @@ PRODUCT_PACKAGES += etc_hosts
 PRODUCT_PACKAGES += init.zygote32.rc
 
 PRODUCT_SYSTEM_PROPERTIES += debug.atrace.tags.enableflags=0
-PRODUCT_SYSTEM_PROPERTIES += persist.traced.enable=1
+PRODUCT_SYSTEM_PROPERTIES += persist.traced.enable=0
 PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.game_default_frame_rate_override=60
 
 # When the flag RELEASE_ADBD_OPEN_VSOCK_PORT is enabled, open adbd on vsock port 8382 as default.
